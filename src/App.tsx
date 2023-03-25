452d65ddc759205b35d7ef1090d47f8551213880
devGEP <ju0032564@gmail.com>
2023-03-26 00:21:29 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
