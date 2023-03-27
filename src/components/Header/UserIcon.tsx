37de9325d8bd1a34216a791235a0bc78b2e7caec
devGEP <ju0032564@gmail.com>
2023-03-27 22:26:52 +0900

    feat: header 추가
    web logo & user icon

---
+++ src/components/Header/UserIcon.tsx
@@ -0,0 +1,30 @@
+import { 
+    Link,
+    Box,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import '../../fonts/font.css';
+
+export default function UserIcon(){
+    return(
+        <Box sx={{ mb: '-100px', mr: '120px' }}>
+            <Link
+                fontFamily= 'BalooBhaijaan'
+                sx={{ 
+                    color: "#FFFFFF",
+                    "&:hover": {
+                    color: "#7673DC"
+                    },
+                    fontWeight: "bold", fontSize: "40px"
+                }}
+                align="right"
+                variant="h5"
+                href="/"
+                underline="none"
+            >
+                    <AccountCircleIcon fontSize='large' sx={{mb: -0.5,mr: 1}}/>
+                    User
+            </Link>
+        </Box>
+    )
+}
