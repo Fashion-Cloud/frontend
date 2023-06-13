e21488bccb3856c2e917a15731e2535eba325303
Ray Lee <wooo519@naver.com>
2023-06-14 00:01:23 +0900

    fix : app router 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DemoPage from "./pages/DemoPage";
-import FirstPage from "./pages/Firstpage";
+import FirstPage from "../src/pages/FirstPage";
 import MainPage from "./pages/MainPage";
 
 export default function App() {
-
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<FirstPage/>}/>
-          <Route path="/main" element={<MainPage/>}/>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/main" element={<MainPage />} />
           {/* <Route path="/demo" element={<DemoPage/>}/> */}
         </Routes>
       </Router>
