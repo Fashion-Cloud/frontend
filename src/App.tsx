b1517a8a7ab35cdf8ca13d0a04bde3b09ca493a8
Ray Lee <wooo519@naver.com>
2023-06-21 01:07:30 +0900

    fix : app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Mainpage from "./pages/Mainpage";
-import {RecoilRoot} from 'recoil';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mainpage from "./pages/MainPage";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
-
   return (
     <RecoilRoot>
       <Router>
         <Routes>
-          <Route path="/" element={<Mainpage/>}/>
+          <Route path="/" element={<Mainpage />} />
         </Routes>
       </Router>
     </RecoilRoot>
