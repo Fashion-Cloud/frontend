2287f34bd05b943f29bf4234860382067b5fb661
HyunTaek Oh <oht222@naver.com>
2023-09-19 02:40:44 +0900

    add: 쿼리클라이언트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
-import {RecoilRoot} from 'recoil';
-
-import MainDrawer from "./components/common/MainDrawer";
-import LookbookDetailpage from "./pages/LookbookDetailpage";
-import Lookbookpage from "./pages/Lookbookpage";
-import Mainpage from "./pages/MainPage";
+import MainDrawer from './components/common/MainDrawer';
+import LookbookDetailpage from './pages/LookbookDetailpage';
+import Lookbookpage from './pages/Lookbookpage';
+import Mainpage from './pages/MainPage';
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <RecoilRoot>
-      <MainDrawer/>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Mainpage/>}/>
-          <Route path="/lookbook" element={<Lookbookpage/>}/>
-          <Route path="/lookbookdetail" element={<LookbookDetailpage/>}/>
-        </Routes>
-      </Router>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <MainDrawer />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Mainpage />} />
+            <Route path="/lookbook" element={<Lookbookpage />} />
+            <Route path="/lookbookdetail" element={<LookbookDetailpage />} />
+          </Routes>
+        </Router>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
